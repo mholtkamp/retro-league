f30b8ac89647cf7306b9c812754c72077204b66d
Martin Holtkamp <mholt012@gmail.com>
2023-07-12 20:32:04 -0400

    Add android project

---
+++ Rocket/Android/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_bonedust_rocket_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Project Rocket!";
+    return env->NewStringUTF(hello.c_str());
+}
